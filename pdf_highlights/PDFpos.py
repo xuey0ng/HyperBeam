11724d28c633acb8d61e89fdb53460be57a859dc
Xue Yong <xueyong26@hotmail.com>
2020-07-05 15:03:29 +0800

    Modified PDFPos.py to support more complicated pdf layouts

--- pdf_highlights/PDFpos.py
+++ pdf_highlights/PDFpos.py
@@ -15,24 +15,27 @@ from pdf_highlights.TextStore import Token
 ## Program returns the an array containing all of the coords of the pdf to be preprocessed
 
 
-#consider comparing base on creationg date of the pdf
-
 class PDFpos:
     
     def __init__(self, filename):
         self.word_array = list()
         self.filename = filename
         self.hashed = ''
+        self.current = ""
+        self.x1 = -1
+        self.x2 = -1
+        self.y1 = -1
+        self.y2 = -1
+        self.pageno = -1
 
     def parse_page(self, lt_page, pageno):
-
         # loop over the object list
         for obj in lt_page:
             if isinstance(obj, pdfminer.layout.LTTextLine):
                 self.parse_line(obj, pageno)
 
             # if it's a textbox, also recurse
-            if isinstance(obj, pdfminer.layout.LTTextBoxHorizontal):
+            if isinstance(obj, pdfminer.layout.LTTextBoxHorizontal) or isinstance(obj, pdfminer.layout.LTTextBox):
                 if pageno == 1 or pageno == '1':
                     hash_obj = hashlib.md5(str(obj.get_text()).encode())
                     self.hashed = str(hash_obj.hexdigest())
@@ -41,39 +44,49 @@ class PDFpos:
             # if it's a container, recurse
             elif isinstance(obj, pdfminer.layout.LTFigure):
                 self.parse_page(obj._objs, pageno)
+                
+            elif isinstance(obj, pdfminer.layout.LTChar):
+                self.parse_char(obj, pageno)
     
     def parse_line(self, lt_line, pageno):
-        current = ""
-        x1 = -1
-        x2 = -1
-        y1 = -1
-        y2 = -1
-        #print(str(lt_line.bbox[0]) + " | "+ str(lt_line.bbox[2]) + " | "+str(lt_line.bbox[1]) + " | "+str(lt_line.bbox[3]))
+        # Allows for recursion if the text is stored in a line
         for obj in lt_line:
-            if isinstance(obj, pdfminer.layout.LTText) and not isinstance(obj, pdfminer.layout.LTAnno):
-                # print("%6d, %6d, %s" % (obj.bbox[0], obj.bbox[1], obj.get_text().replace('\n', '_')))
-                thisword = obj.get_text()
-                if thisword == '\n' or thisword == ' ':
-                    temp = Token(pageno, x1, x2, y1, y2, current, self.filename, self.hashed)
-                    self.word_array.append(temp)
-                    current = ""
-                    x1 = -1
-                    x2 = -1
-                    y1 = -1
-                    y2 = -1
-                elif x1 == -1:
-                    x1 = obj.bbox[0]
-                    x2 = obj.bbox[2]
-                    y1 = obj.bbox[1]
-                    y2 = obj.bbox[3]
-                    # current_x = (obj.bbox[0] + obj.bbox[2])/2
-                    # current_y = (obj.bbox[1])
-                    current += thisword
-                else:
-                    current += thisword
-                    x2 = obj.bbox[2]
-            # elif isinstance(obj, pdfminer.layout.LTChar):
-                # print("%6d, %6d, %s" % (obj.bbox[0], obj.bbox[1], obj.get_text().replace('\n', '_')))
+            if isinstance(obj, pdfminer.layout.LTChar)  and not isinstance(obj, pdfminer.layout.LTAnno):
+                self.parse_char(obj, pageno)
+
+    def parse_char(self, obj, pageno):
+        if pageno != self.pageno:
+            if self.x1 != -1:
+                temp = Token(self.pageno, self.x1, self.x2, self.y1, self.y2, self.current, self.filename, self.hashed)
+                self.word_array.append(temp)
+                self.current = ""
+            self.x1 = -1
+            self.x2 = -1
+            self.y1 = -1
+            self.y2 = -1
+            self.pageno = -1
+        thisword = obj.get_text()
+        if thisword == '\n' or thisword == ' ' or thisword == '.' or thisword == ',' or thisword == '—' or thisword == '-':
+            if self.x1 != -1:
+                temp = Token(self.pageno, self.x1, self.x2, self.y1, self.y2, self.current, self.filename, self.hashed)
+                self.word_array.append(temp)
+            self.current = ""
+            self.x1 = -1
+            self.x2 = -1
+            self.y1 = -1
+            self.y2 = -1
+            self.pageno = -1
+        elif self.x1 == -1:
+            self.x1 = obj.bbox[0]
+            self.x2 = obj.bbox[2]
+            self.y1 = obj.bbox[1]
+            self.y2 = obj.bbox[3]
+            self.current += thisword
+            self.pageno = pageno
+        else:
+            self.current += thisword
+            self.x2 = obj.bbox[2]
+            
 
     def parsepdf(self):
         # Open a PDF file.
@@ -85,9 +98,9 @@ class PDFpos:
         # Create a PDF document object that stores the document structure.
         # Password for initialization as 2nd parameter
         document = PDFDocument(parser)
-
         # Check if the document allows text extraction. If not, abort.
         if not document.is_extractable:
+            print('extraction not allowed')
             raise PDFTextExtractionNotAllowed
 
         # Create a PDF resource manager object that stores shared resources.
@@ -110,15 +123,16 @@ class PDFpos:
         i = 0
         # loop over all pages in the 
         for page in PDFPage.create_pages(document):
-            #print(page.mediabox)
+            # int i to keep track of page numbers
             i+=1
+
             # read the page into a layout object
             interpreter.process_page(page)
             layout = device.get_result()
 
             # extract text from this object
             self.parse_page(layout._objs, i)
-            return self.word_array
+        return self.word_array
     
 
 #test = PDFpos("FinancialAccounting1.pdf")
