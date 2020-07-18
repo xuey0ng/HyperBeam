f84fb0228d63d10016c20582b8efc1f9854d85cb
Trav1sT <guanw998@gmail.com>
2020-07-18 15:10:30 +0800

    Add next task in 'At a glance'

--- lib/widgets/atAGlance.dart
+++ lib/widgets/atAGlance.dart
@@ -19,114 +19,124 @@ class AtAGlance extends StatefulWidget {
 class _AtAGlanceState extends State<AtAGlance> {
   final controller = PageController();
   List<Widget> _pages;
-  void initPage(List<DocumentSnapshot> list) {
-    if(list.length == 0){
-      _pages = <Widget>[
-        new ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Container(
-              padding: EdgeInsets.only(left: 8,top: 8),
-              child: RichText(
-                  text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: "You have 0 task(s) coming up",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: kMediumText
-                            )
-                        )
-                      ]
-                  )
-              )
-          ),
-        ),
-        new ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Container(
-              padding: EdgeInsets.only(left: 8,top: 8),
-              child: RichText(
-                  text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: "Next task",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: kMediumText
-                            )
-                        ),
-                        TextSpan(text: "task1"),
-                        TextSpan(text: "dateTime"),
-                      ]
-                  )
-              )
-          ),
-        ),
-      ];
-    } else {
-      DocumentSnapshot snap = list[0];
-      print("snap is ${snap.data}");
-      _pages = <Widget>[
-        new ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Container(
-              padding: EdgeInsets.only(left: 4,top: 4),
-              child: RichText(
-                  text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: "Next quiz:    ",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: kMediumText
-                            )
-                        ),
-                        TextSpan(
-                            text: "${snap.data["quizName"]}\n\n",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: kMediumText
-                            )
-                        ),
-                        TextSpan(
-                            text: "Reminder set at:  ${DateFormat('dd-MM-yyyy  kk:mm').format(snap.data['date'].toDate())}",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: kMediumText
-                            )
-                        ),
-                      ]
-                  )
-              )
-          ),
-        ),
-        new ConstrainedBox(
-          constraints: const BoxConstraints.expand(),
-          child: Container(
-              padding: EdgeInsets.only(left: 8,top: 8),
-              child: RichText(
-                  text: TextSpan(
-                      children: [
-                        TextSpan(
-                            text: "Next task reminder",
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: kMediumText
-                            )
-                        ),
-                        TextSpan(text: "task1"),
-                        TextSpan(text: "dateTime"),
-                      ]
-                  )
-              )
-          ),
-        ),
-      ];
+  void initPage(List<DocumentSnapshot> list, List<DocumentSnapshot> list2) {
+    DocumentSnapshot snap;
+    DocumentSnapshot snap2;
+    if(list.length != 0){
+      snap = list[0];
     }
-
+    if(list2.length != 0){
+      snap2 = list2[0];
+    }
+    _pages = <Widget>[
+      list.length == 0 ? ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: Container(
+            padding: EdgeInsets.only(left: 8,top: 8),
+            child: RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "You have 0 quizzes coming up",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      )
+                    ]
+                )
+            )
+        ),
+      ) :
+      ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: Container(
+            padding: EdgeInsets.only(left: 4,top: 4),
+            child: RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "Next quiz:    ",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      ),
+                      TextSpan(
+                          text: "${snap.data["quizName"]}\n\n",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      ),
+                      TextSpan(
+                          text: "Reminder set at:  ${DateFormat('dd-MM-yyyy  kk:mm')
+                              .format(snap.data['date'].toDate())}",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      ),
+                    ]
+                )
+            )
+        ),
+      ),
+      list2.length == 0 ? ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: Container(
+            padding: EdgeInsets.only(left: 8,top: 8),
+            child: RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "You have 0 tasks coming up",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      )
+                    ]
+                )
+            )
+        ),
+      ) :
+      ConstrainedBox(
+        constraints: const BoxConstraints.expand(),
+        child: Container(
+            padding: EdgeInsets.only(left: 4,top: 4),
+            child: RichText(
+                text: TextSpan(
+                    children: [
+                      TextSpan(
+                          text: "Next task:    ",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      ),
+                      TextSpan(
+                          text: "${snap2.data["taskName"]}\n\n",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      ),
+                      TextSpan(
+                          text: "Reminder set at:  ${DateFormat('dd-MM-yyyy  kk:mm')
+                              .format(snap2.data['date'].toDate())}",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: kMediumText
+                          )
+                      ),
+                    ]
+                )
+            )
+        ),
+      )
+    ];
   }
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -137,56 +147,68 @@ class _AtAGlanceState extends State<AtAGlance> {
       builder: (context, snapshot) {
         if (!snapshot.hasData) return LinearProgressIndicator();
         List<DocumentSnapshot> list = snapshot.data.documents;
-        initPage(list);
-        return Stack(
-            children: [
-              Opacity(
-                opacity: 0.6,
-                child: Container(
-                  margin: EdgeInsets.only(left: widget.screenWidth*0.1, top: 16),
-                  padding: EdgeInsets.only(left: 10,top: 10),
-                  height: widget.screenHeight * 0.20,
-                  width: widget.screenWidth * 0.8,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
-                    border: Border.all(width: 2.0, color: kPrimaryColor),
-                    color: kPrimaryColor,
+        return FutureBuilder<QuerySnapshot>(
+          future: Firestore.instance.collection("TaskReminders").where("uid", isEqualTo: user.id).getDocuments(),
+          builder: (context, snapshot2) {
+            if (!snapshot2.hasData) return LinearProgressIndicator();
+            List<DocumentSnapshot> list2 = snapshot2.data.documents;
+            list2.sort((a,b){
+              if( a.data["date"] < b.data["date"]) {
+                return 1;
+              } else {
+                return -1;
+              }
+            });
+            initPage(list, list2);
+            return Stack(
+                children: [
+                  Opacity(
+                    opacity: 0.6,
+                    child: Container(
+                      margin: EdgeInsets.only(left: widget.screenWidth*0.1, top: 16),
+                      padding: EdgeInsets.only(left: 10,top: 10),
+                      height: widget.screenHeight * 0.20,
+                      width: widget.screenWidth * 0.8,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        border: Border.all(width: 2.0, color: kPrimaryColor),
+                        color: kPrimaryColor,
+                      ),
+                    ),
                   ),
-                ),
-              ),
-              Container(
-                  margin: EdgeInsets.only(left: widget.screenWidth*0.1, top: 16),
-                  padding: EdgeInsets.only(left: 10,top: 10),
-                  height: widget.screenHeight * 0.20,
-                  width: widget.screenWidth * 0.8,
-                  child: Stack(
-                    children: <Widget>[
-                      PageView.builder(
-                        physics: new AlwaysScrollableScrollPhysics(),
-                        controller: controller,
-                        itemBuilder: (BuildContext context, int index) {
-                          return _pages[index % _pages.length];
-                        },
-                      ),
-                      Positioned(
-                          bottom: 4,
-                          left: 0.0,
-                          right: 0.0,
-                          child: Center(
-                            child: SmoothPageIndicator(
-                              controller: controller,
-                              count: 2,
-                              effect: WormEffect(),
-                            ),
-                          )
-                      ),
-                    ],
-                  )
-              ),
-            ]
+                  Container(
+                      margin: EdgeInsets.only(left: widget.screenWidth*0.1, top: 16),
+                      padding: EdgeInsets.only(left: 10,top: 10),
+                      height: widget.screenHeight * 0.20,
+                      width: widget.screenWidth * 0.8,
+                      child: Stack(
+                        children: <Widget>[
+                          PageView.builder(
+                            physics: new AlwaysScrollableScrollPhysics(),
+                            controller: controller,
+                            itemBuilder: (BuildContext context, int index) {
+                              return _pages[index % _pages.length];
+                            },
+                          ),
+                          Positioned(
+                              bottom: 4,
+                              left: 0.0,
+                              right: 0.0,
+                              child: Center(
+                                child: SmoothPageIndicator(
+                                  controller: controller,
+                                  count: 2,
+                                  effect: WormEffect(),
+                                ),
+                              )
+                          ),
+                        ],
+                      )
+                  ),
+                ]
+            );
+          }
         );
-
-
       }
     );
 
